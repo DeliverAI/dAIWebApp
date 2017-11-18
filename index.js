6f636ad8bb056ffd76bdf1a1c4d485e224c15c23
Michael <mike.l_42@hotmail.com>
2017-11-18 18:47:57 -0500

    90% Finished Prototype
    *Shopping page layout done and images filled
    *Shopping pages send data to firebase
    *Link and communication between sign up and shopping

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
-
-
-
-$(document).ready(function(){
+$(document).ready(function() {
   var pictureTaken = false;
   $('#show_stream').hide();
 });
 
-  function firebaseSaveData(){
-    var name = document.getElementById("name");
-    var address = document.getElementById("address");
-    var phone = document.getElementById("phone");
-    var name_value = name.value;
-    var address_value = address.value;
-    var phone_value = phone.value;
-    var firebaseRef = firebase.database().ref();
-    var usersRef = firebaseRef.child("users");
+function sleep(ms) {
+  return new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
+}
 
-    usersRef.push().set({
-      name: name_value,
-      address: address_value,
-      phone_number: phone_value
-    });
+async function firebaseSaveData() {
+  var name = document.getElementById("name");
+  var address = document.getElementById("address");
+  var phone = document.getElementById("phone");
+  var name_value = name.value;
+  var address_value = address.value;
+  var phone_value = phone.value;
+  var firebaseRef = firebase.database().ref();
+  var usersRef = firebaseRef.child("users");
 
-    var whoOrderedRef = firebaseRef.child("who_ordered");
-    whoOrderedRef.set({
-      name: name_value,
-      phone_number: phone_value,
-      address: address_value,
-      order_made: false
-    });
+  usersRef.push().set({
+    name: name_value,
+    address: address_value,
+    phone_number: phone_value
+  });
+
+  var whoOrderedRef = firebaseRef.child("who_ordered");
+  whoOrderedRef.set({
+    name: name_value,
+    phone_number: phone_value,
+    address: address_value,
+    order_made: false
+  });
+  await sleep(1200);
+  document.location.href = "shopping/shopping.html";
+}
+
+function whichButton(state) {
+  if (state == "snap") {
+    $('#take_snapshots').hide();
+    $('.webcamBtn').hide();
+    $('#show_stream').show();
+    $('.repeatBtn').show();
+  } else if (state == "stream") {
+    $('#take_snapshots').show();
+    $('.webcamBtn').show();
+    $('#show_stream').hide();
+    $('.repeatBtn').hide();
   }
-
-  function whichButton(state){
-    if (state == "snap"){
-      $('#take_snapshots').hide();
-      $('.webcamBtn').hide();
-      $('#show_stream').show();
-      $('.repeatBtn').show();
-    }else if (state == "stream"){
-      $('#take_snapshots').show();
-      $('.webcamBtn').show();
-      $('#show_stream').hide();
-      $('.repeatBtn').hide();
-    }
-  }
+}
