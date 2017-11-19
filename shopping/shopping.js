c186c9cf80454b0894bb1320d7af22f7f8448922
Michael <mike.l_42@hotmail.com>
2017-11-18 22:27:37 -0500

    Refined shopping page
    *Shopping adjustments
    *Created base confirmation page

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,9 +1,16 @@
-function firebaseShop(){
+function sleep(ms) {
+  return new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
+}
+
+async function firebaseShop(){
 
   var firebaseRef = firebase.database().ref();
   var whoOrderedRef = firebaseRef.child("who_ordered");
   whoOrderedRef.update({
     order_made: true
   });
-
+  await sleep(1200);
+  document.location.href = "shopping/shopping.html";
 }
